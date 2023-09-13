4bd5df3caf58d0ffab8978c94e3d9d6f474d7ce8
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-13 13:27:25 +0000

    Types

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type DropEventHandler = (ev: React.DragEvent<HTMLTableDataCellElement>) => void;
+type DragOverEventHandler = (ev: React.DragEvent<HTMLTableDataCellElement>) => void;
+type DragStartEventHandler = (ev: React.DragEvent<HTMLImageElement>) => void;
+
+export type SquareProps = {
+  id: string;
+  onDrop: DropEventHandler;
+  onDragOver: DragOverEventHandler;
+  pieceId?: string;
+}
+
+export type PieceProps = {
+  id: string;
+  onDragStart: DragStartEventHandler;
+}
+
+export type BoardState = {
+  [key: string]: [number, number, string]
+}
