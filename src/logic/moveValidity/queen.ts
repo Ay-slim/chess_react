e8ce7bfae69d7e9128025719345b226a9e8834cc
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-15 03:06:51 +0000

    Add all threatened squares to queen validity function

--- src/logic/moveValidity/queen.ts
+++ src/logic/moveValidity/queen.ts
@@ -2,9 +2,9 @@ import { default as bishopValidity } from './bishop'
 import { default as rookValidity } from './rook'
 import { BoardState, PlayerColor } from "../../types"
 
-const validSquares = (squareId: string, color: PlayerColor, boardState: BoardState): string[] => {
-  const bishopThreats: string[] = bishopValidity(squareId, color, boardState);
-  const rookThreats: string[] = rookValidity(squareId, color, boardState);
+const validSquares = (squareId: string, color: PlayerColor, boardState: BoardState, allThreatenedSquares: boolean = false): string[] => {
+  const bishopThreats: string[] = bishopValidity(squareId, color, boardState, allThreatenedSquares);
+  const rookThreats: string[] = rookValidity(squareId, color, boardState, allThreatenedSquares);
   return bishopThreats.concat(rookThreats);
 }
 
