a81c0314d1e88c68fec4f1ee6198aa51e8432dff
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-23 15:29:58 +0000

    Pass down king in check state and set state as props

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -4,8 +4,8 @@ import { drop } from "../logic/handlers";
 import '../App.css';
 
 const Square = (props: SquareProps) => {
-  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare} = props
-  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare)
+  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck} = props
+  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck)
   return (
     <th id={id} className={id} onDrop={handleDrop} onDragOver={onDragOver}>
       {pieceId ? <Piece id={pieceId} currentColor={currentColor} squareId={id}/>: ''}
