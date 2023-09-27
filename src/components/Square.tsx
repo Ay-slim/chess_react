2b32c1477f913000ce3b5421e84ea62f5899df6c
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-27 08:29:52 +0000

    Pass down checkmate state and setter to square component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -4,11 +4,11 @@ import { drop } from "../logic/handlers";
 import '../App.css';
 
 const Square = (props: SquareProps) => {
-  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck} = props
-  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck)
+  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate} = props
+  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate)
   return (
     <th id={id} className={id} onDrop={handleDrop} onDragOver={onDragOver}>
-      {pieceId ? <Piece id={pieceId} currentColor={currentColor} squareId={id}/>: ''}
+      {pieceId ? <Piece id={pieceId} currentColor={currentColor} squareId={id} checkMate={checkMate}/>: ''}
     </th>
   );
 }
