0fe1f4c816f016ef8440e4f604d0a58caf0b35a5
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-27 18:01:39 +0000

    Remove destination square requirement in King logic

--- src/logic/moveValidity/kingAug.ts
+++ src/logic/moveValidity/kingAug.ts
@@ -4,21 +4,24 @@ import { CASTLING_ROOKS_DICT } from "../utils";
 import { default as kingValidSquares } from './king'
 import { allThreatenedSquares } from ".";
 
-const allValidKingSquares = (squareId: string, color: PlayerColor, boardState: BoardState, destSquare: string, gameMoves: MoveHistoryType[]) => {
+const allValidKingSquares = (squareId: string, color: PlayerColor, boardState: BoardState, gameMoves: MoveHistoryType[]) => {
   const allSquaresUnderAttack = allThreatenedSquares(color, boardState)
-  const xCoord = boardState[squareId].loc[0];
-  const xCoordTarget = boardState[destSquare].loc[0];
-  const xDiff = xCoordTarget - xCoord;
-  let targetCastlingSquare = ''
-  if ([2, -2].includes(xDiff)) {
+  const targetCastlingSquares = []
+  for (const xDiff of [2, -2]) {
     const castlingDirection = xDiff === 2 ? '+ve' : '-ve';
     const rookToCastleWith = CASTLING_ROOKS_DICT[`${color}${castlingDirection}`];
     const movedPieces = gameMoves.map((move) => move.piece);
     const kingOrRookAlreadyMoved = movedPieces.includes(`${color}k`) || movedPieces.includes(rookToCastleWith);
-    targetCastlingSquare = kingOrRookAlreadyMoved ? '' : castlingCheck(color, castlingDirection, boardState, allSquaresUnderAttack)
+    if (kingOrRookAlreadyMoved) {
+      continue
+    }
+    const targetCastlingSquare = castlingCheck(color, castlingDirection, boardState, allSquaresUnderAttack)
+    if (targetCastlingSquare) {
+      targetCastlingSquares.push(targetCastlingSquare)
+    }
   }
   const kingsSquares = kingValidSquares(squareId, color, boardState)
-  const allKingsSquares = targetCastlingSquare ? [targetCastlingSquare].concat(kingsSquares) : kingsSquares
+  const allKingsSquares = targetCastlingSquares.length ? targetCastlingSquares.concat(kingsSquares) : kingsSquares
   const filteredKSquares = allKingsSquares.filter(square => !allSquaresUnderAttack.includes(square))
   return filteredKSquares
 }
