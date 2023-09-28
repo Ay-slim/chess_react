66250e03abecb7f3dc5763f19aaa2f28ba1619e0
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-28 07:58:07 +0000

    Stalemate implementation on square component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -4,11 +4,11 @@ import { drop } from "../logic/handlers";
 import '../App.css';
 
 const Square = (props: SquareProps) => {
-  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate} = props
-  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate)
+  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate, staleMate, setStaleMate} = props
+  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate, staleMate, setStaleMate)
   return (
     <th id={id} className={id} onDrop={handleDrop} onDragOver={onDragOver}>
-      {pieceId ? <Piece id={pieceId} currentColor={currentColor} squareId={id} checkMate={checkMate}/>: ''}
+      {pieceId ? <Piece id={pieceId} currentColor={currentColor} squareId={id} checkMate={checkMate} staleMate={staleMate}/>: ''}
     </th>
   );
 }
