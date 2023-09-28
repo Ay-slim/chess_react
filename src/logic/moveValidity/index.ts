fda3fbb881e3c85091daa2d184eb029ad7525726
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-28 08:01:40 +0000

    All valid move functions to implement stalemate

--- src/logic/moveValidity/index.ts
+++ src/logic/moveValidity/index.ts
@@ -1,4 +1,4 @@
-import { BoardState, MoveHistoryType, PieceValidityTypes, PlayerColor, PieceNameType, KingCheckType, CheckMateType, SetCheckMateType } from "../../types";
+import { BoardState, MoveHistoryType, PieceValidityTypes, PlayerColor, PieceNameType, KingCheckType, CheckMateType, SetCheckMateType, SourceSquareAndValidMovesType } from "../../types";
 
 import { default as corePawnValidity } from './pawn'
 import { default as pawnValidity } from './pawnAug'
@@ -9,6 +9,14 @@ import { default as queenValidity } from './queen'
 import { default as kingValidity } from './kingAug'
 import { default as coreKingValidity } from './king'
 import { default as generatePinnedSquares } from "../pinnedSquares"
+
+
+const validityMap = {
+  n: knightValidity,
+  r: rookValidity,
+  b: bishopValidity,
+  q: queenValidity,
+}
 
 export const moveValidityCheck = (srcSquareId: string, destSquareId: string, color: PlayerColor, boardState: BoardState, gameMoves: MoveHistoryType[], pieceName: string, kingSquare: string, kingInCheck: KingCheckType, checkMate: CheckMateType, setCheckMate: SetCheckMateType) => {
   if (kingInCheck.color) {
@@ -27,20 +35,32 @@ export const moveValidityCheck = (srcSquareId: string, destSquareId: string, col
     return false
   }
 
-  const validityMap = {
-    n: knightValidity,
-    r: rookValidity,
-    b: bishopValidity,
-    q: queenValidity,
-  }
-
   if (pieceName === 'p') {
     return pawnValidity(srcSquareId, color, boardState, gameMoves).includes(destSquareId);
   } else if (pieceName === 'k') {
-    return kingValidity(srcSquareId, color, boardState, destSquareId, gameMoves).includes(destSquareId);
+    return kingValidity(srcSquareId, color, boardState, gameMoves).includes(destSquareId);
   } else {
     return validityMap[pieceName as PieceValidityTypes](srcSquareId, color, boardState).includes(destSquareId);
   }
+}
+
+export const allValidMoves = (color: PlayerColor, boardState: BoardState, pinnedSquares: SourceSquareAndValidMovesType, movesHistory: MoveHistoryType[]) => {
+  const allValidMovesMap: SourceSquareAndValidMovesType = {}
+  const pinnedSquaresArr = Object.keys(pinnedSquares)
+  for (let currentSquare in boardState) {
+    const currentPiece = boardState[currentSquare]?.piece
+    const currentPieceName = currentPiece[1]
+    if (currentPiece[0] === color && !pinnedSquaresArr.includes(currentSquare)) {
+      const validSquares = currentPieceName === 'p' ? pawnValidity(currentSquare, color, boardState, movesHistory)
+       : currentPieceName === 'k' ? kingValidity(currentSquare, color, boardState, movesHistory) 
+       :  validityMap[currentPieceName as PieceValidityTypes](currentSquare, color, boardState)
+      allValidMovesMap[currentSquare] = {
+        piece: boardState[currentSquare].piece,
+        validSquares
+      }
+    }
+  }
+  return {...allValidMovesMap, ...pinnedSquares}
 }
 
 export const allThreatenedSquares = (color: PlayerColor, boardState: BoardState) => {
