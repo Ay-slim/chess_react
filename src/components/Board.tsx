27d8b4f3e3206b426c17a9a00358bfaa4a912ddc
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-03 14:27:38 +0000

    Fifty moves tracker state init and pass props

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -102,6 +102,7 @@ const Board = () => {
     w: ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
     b: ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8']
   });
+  const [fiftyMovesTracker, setFiftyMovesTracker] = useState<number>(0);
 
   return (
     <div className="container">
@@ -142,6 +143,8 @@ const Board = () => {
                   setValidMoves={setValidMoves}
                   occupiedSquares={occupiedSquares}
                   setOccupiedSquares={setOccupiedSquares}
+                  fiftyMovesTracker={fiftyMovesTracker}
+                  setFiftyMovesTracker={setFiftyMovesTracker}
                 />
               );
             })}
