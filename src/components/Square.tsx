502e2e5b1bfe2c25a6053b6d563047fbc9867372
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-03 14:28:12 +0000

    Fifty moves tracker Square component pass props

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -4,8 +4,8 @@ import { drop } from "../logic/handlers";
 import '../App.css';
 
 const Square = (props: SquareProps) => {
-  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate, staleMate, setStaleMate, validMoves, setValidMoves, occupiedSquares, setOccupiedSquares} = props
-  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, setCheckMate, setStaleMate, validMoves, setValidMoves, occupiedSquares, setOccupiedSquares)
+  const {id, onDragOver, pieceId, currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, checkMate, setCheckMate, staleMate, setStaleMate, validMoves, setValidMoves, occupiedSquares, setOccupiedSquares, fiftyMovesTracker, setFiftyMovesTracker} = props
+  const handleDrop = drop(currentColor, setColor, currentBoard, setBoardState, setAlertMessage, movesHistory, setMoveHistory, capturedPieces, setCapturedPiece, kingSquare, setKingSquare, kingInCheck, setKingInCheck, setCheckMate, setStaleMate, validMoves, setValidMoves, occupiedSquares, setOccupiedSquares, fiftyMovesTracker, setFiftyMovesTracker)
   return (
     <th id={id} className={id} onDrop={handleDrop} onDragOver={onDragOver}>
       {pieceId ? <Piece id={pieceId} currentColor={currentColor} squareId={id} checkMate={checkMate} staleMate={staleMate}/>: ''}
