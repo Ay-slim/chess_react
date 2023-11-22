f221963a2b0dd53d11138374840416427057b37f
Ay-slim <ayooluwaadedipe@gmail.com>
2023-11-22 12:02:18 +0000

    Load routes with base_url rather than relative paths (prevent breaking when routes include params)

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -5,7 +5,7 @@ import { drag } from '../logic/handlers'
 const Piece = (props: PieceProps) => {
   const { id, squareId, currentColor, checkMate, staleMate } = props
   const handleDrag = drag(squareId)
-  const src = `${id.substring(0, 2)}.png`
+  const src = `${process.env.REACT_APP_BASE_URL}${id.substring(0, 2)}.png`
   const multiPlayerColor = sessionStorage.getItem('multiPlayerColor')
   const notMyTurn = multiPlayerColor && multiPlayerColor !== currentColor
   const draggable = checkMate || staleMate || notMyTurn ? false : currentColor === id[0]
