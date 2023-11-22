443242ccb033cebd23b446619d495eb1de1a8160
Ay-slim <ayooluwaadedipe@gmail.com>
2023-11-22 11:59:36 +0000

    Set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import MainPage from './components/MainPage'
+import Board from './components/Board'
+import AwaitingOpponent from './components/AwaitingOpponent'
 import MultiplayerBoard from './components/MultiplayerBoard'
+import { Routes, Route } from 'react-router-dom'
 const App = () => {
-  return <MultiplayerBoard />
+  return (
+    <>
+      <Routes>
+        <Route path='/' Component={MainPage} />
+        <Route path='/single' Component={Board} />
+        <Route path='/awaiting' Component={AwaitingOpponent} />
+        <Route path='/multiboard' Component={MultiplayerBoard} />
+        <Route path='/multi/:gameIds' Component={MultiplayerBoard} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
